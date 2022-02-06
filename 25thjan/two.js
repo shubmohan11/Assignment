4a57a60b4fa12f496cddf2a24541c79b589f8946
shubmohan11 <30762257+shubmohan11@users.noreply.github.com>
2022-02-06 18:31:55 +0530

    Create two.js

---
+++ 25thjan/two.js
@@ -0,0 +1,11 @@
+var a=10;
+var b=20;
+var c=30;
+
+if(a>b && a>c){
+    console.log("The greater num is a= ",a);
+}else if(b>a && b>c){
+    console.log("The greater num is b= ",b);
+}else{
+    console.log("The greater num is c=",c);
+}
