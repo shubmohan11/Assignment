940ab10ff6fbcec7b045a8511d153b887b47f06b
shubmohan11 <30762257+shubmohan11@users.noreply.github.com>
2022-03-06 15:15:52 +0530

    Create assignment2.js

---
+++ feb28tomar6/Mar3rd/assignment2.js
@@ -0,0 +1,10 @@
+var a=prompt("Enter the value of 1st");
+var b=prompt("Enter the value of 2nd");
+
+a=parseInt(a);
+b=parseInt(b);
+
+a=a+b;
+b=a-b;
+a=a-b;
+document.write("Value of a ="+a,"    " +"Value of b ="+b);
